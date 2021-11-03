373da969fad3f78cc5c34eaf8212f5e238cc2b01
Isack <ituwei21@gmail.com>
2021-11-03 11:48:22 +0300

    set up h2 db

--- src/main/java/com/ituwei/app/Model/User.java
+++ src/main/java/com/ituwei/app/Model/User.java
@@ -1,6 +1,9 @@
 package com.ituwei.app.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +14,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
     private String password;
